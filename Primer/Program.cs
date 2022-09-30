c40ae20a1fd196c67242b05d374c948f5b0dc5ed
Maria Yutaeva <mariya1982@yandex.ru>
2022-09-30 10:09:32 +0300

    пример с методом

---
+++ Primer/Program.cs
@@ -0,0 +1,10 @@
+﻿int GetMulti(int a, int b)
+{
+    return a*b;
+}
+
+Console.WriteLine("Введите число А: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Произведение чисел А и В = " + GetMulti(num1, num2));
