8cdcb3b47a9a8a192bd692cca14eb72704a446d9
Maria Yutaeva <mariya1982@yandex.ru>
2022-10-03 18:14:34 +0300

    задача35

---
+++ Task35/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 35. Задайте одномерный массив, состоящий из случайных чисел.Найдите кол-во элементов массива, 
+// значения которых лежат в отрезке [10,99].
+
+void FillArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(0, 99);
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+    Console.WriteLine();
+}
+int FindCount(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] >= 10 && arr[i] <= 100)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int[] massive = new int[12];
+FillArray(massive);
+PrintArray(massive);
+Console.WriteLine("Число элементов в диапозоне от 10 до 99 = " + FindCount(massive));
